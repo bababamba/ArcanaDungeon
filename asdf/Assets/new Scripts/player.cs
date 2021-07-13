2ee642225c90c945e5651b0401f9b9d1e508dc67
bababamba <86463743+bababamba@users.noreply.github.com>
2021-07-13 19:32:31 +0900

    시야랑 플레이어 좌표 연동
    그리고 플레이어 움직임 관련 변수 public화

--- asdf/Assets/new Scripts/player.cs
+++ asdf/Assets/new Scripts/player.cs
@@ -16,7 +16,8 @@ public class player : Thing
 
     private int hand_limit; //패 장수 제한
 
-    public float Movepower;
+    public float MovePower = 0.2f;
+    public int MoveTimerLimit = 5;
     public SpriteRenderer spriteRenderer;
     public Animator anim;
     public Rigidbody2D rigid;
@@ -25,7 +26,9 @@ public class player : Thing
     public Vector2 PlayerPos = new Vector2(0,0);
     public Vector2 MoveVector = new Vector2(0, 0);
     public Level l;
-    int movetimer = 0;
+
+
+    int MoveTimer = 0;
     int dir = 0;
 
 
@@ -67,24 +70,31 @@ public class player : Thing
         return l.map[(int)a];//jabassibo
 
     }
-    
+    public int whereishe()
+    {
+        int here=0;
+        here = (int)Mathf.Round(rigid.position.y + 1);
+        here *= -l.levelr.Width();
+        here += (int)Mathf.Round(rigid.position.x + 1);
+        return here;
+    }
 
     private void FixedUpdate()
     {
-        if (movetimer == 0)
+        if (MoveTimer == 0)
         {
 
             //left
             if (Input.GetAxisRaw("Horizontal") == -1 && !anim.GetBool("iswalking"))
             {
-                vision_marker();//★나중에 플레이어의 Turn() 함수가 완성되면 그쪽에서 1번만 실행되게 옮길 것, 아래의 같은 함수들도 동일
+                
                 PlayerPos = new Vector2(Mathf.Round(rigid.position.x - 1), Mathf.Round(rigid.position.y));
                 Debug.Log(PlayerPos);
                 if (CheckLevel(PlayerPos.x, PlayerPos.y) != 2)
                 {
 
                     anim.SetBool("iswalking", true);
-                    movetimer = 10;
+                    MoveTimer = MoveTimerLimit;
 
                     dir = 1;
                 }
@@ -92,7 +102,7 @@ public class player : Thing
             //right
             else if (Input.GetAxisRaw("Horizontal") == 1 && !anim.GetBool("iswalking"))
             {
-                vision_marker();//★
+                
 
 
                 PlayerPos = new Vector2(Mathf.Round(rigid.position.x + 1), Mathf.Round(rigid.position.y));
@@ -100,7 +110,7 @@ public class player : Thing
                 {
 
                     anim.SetBool("iswalking", true);
-                    movetimer = 10;
+                    MoveTimer = MoveTimerLimit;
 
                     dir = 2;
                 }
@@ -109,7 +119,7 @@ public class player : Thing
             //up
             else if (Input.GetAxisRaw("Vertical") == 1 && !anim.GetBool("iswalking"))
             {
-                vision_marker();//★
+                
 
 
                 PlayerPos = new Vector2(Mathf.Round(rigid.position.x), Mathf.Round(rigid.position.y + 1));
@@ -117,7 +127,7 @@ public class player : Thing
                 {
 
                     anim.SetBool("iswalking", true);
-                    movetimer = 10;
+                    MoveTimer = MoveTimerLimit;
 
                     dir = 3;
                 }
@@ -126,7 +136,7 @@ public class player : Thing
             //down
             else if (Input.GetAxisRaw("Vertical") == -1 && !anim.GetBool("iswalking"))
             {
-                vision_marker();//★
+               
 
 
                 PlayerPos = new Vector2(Mathf.Round(rigid.position.x), Mathf.Round(rigid.position.y - 1));
@@ -134,55 +144,58 @@ public class player : Thing
                 {
 
                     anim.SetBool("iswalking", true);
-                    movetimer = 10;
+                    MoveTimer = MoveTimerLimit;
 
                     dir = 4;
                 }
 
             }
         }
-        if(movetimer>0)
-        movetimer--;
+        if(MoveTimer > 0)
+            MoveTimer--;
         //playermove
         switch (dir)
         {
             case 1:
-                MoveVector = new Vector2(rigid.position.x - 0.2f, rigid.position.y);
+                MoveVector = new Vector2(rigid.position.x - MovePower, rigid.position.y);
                 rigid.position = MoveVector;
                 if (rigid.position.x <= PlayerPos.x)
                 {
                     dir = 0;
                     anim.SetBool("iswalking", false);
                     rigid.position = PlayerPos;
-                    
+                    vision_marker();//★나중에 플레이어의 Turn() 함수가 완성되면 그쪽에서 1번만 실행되게 옮길 것, 아래의 같은 함수들도 동일
                     Debug.Log("2");
                 }
                 break;
             case 2:
-                MoveVector = new Vector2(rigid.position.x + 0.2f, rigid.position.y);
+                MoveVector = new Vector2(rigid.position.x + MovePower, rigid.position.y);
                 rigid.position = MoveVector;
                 if (rigid.position.x >= PlayerPos.x)
                 {
                     rigid.position = PlayerPos;
+                    vision_marker();//★
                     dir = 0;
                     anim.SetBool("iswalking", false);
                 }
                 break;
             case 3:
-                MoveVector = new Vector2(rigid.position.x, rigid.position.y + 0.2f);
+                MoveVector = new Vector2(rigid.position.x, rigid.position.y + MovePower);
                 rigid.position = MoveVector;
                 if (rigid.position.y >= PlayerPos.y) {
                     rigid.position = PlayerPos;
+                    vision_marker();//★
                     dir = 0;
                     anim.SetBool("iswalking", false);
                 }
                 break;
             case 4:
-                MoveVector = new Vector2(rigid.position.x, rigid.position.y - 0.2f);
+                MoveVector = new Vector2(rigid.position.x, rigid.position.y - MovePower);
                 rigid.position = MoveVector;
                 if (rigid.position.y <= PlayerPos.y)
                 {
                     rigid.position = PlayerPos;
+                    vision_marker();//★
                     dir = 0;
                     anim.SetBool("iswalking", false);
                 }
@@ -198,8 +211,10 @@ public class player : Thing
     //★visionchecker을 먼저 실행해 시야에 보이는 부분을 표시하고, Level에 있는 몬스터 배열을 가져와서 좌표를 비교해 몬스터의 위치도 표시하는 함수
     private void vision_marker() {
         FOV = new bool[this.l.length];
-        cur_pos = -l.width *(int)this.transform.position.y  + (int)this.transform.position.x; //★현재 map[]과 player에서 사용하는 좌표가 서로 다르다, 나중에 통합하고 좌표 갱신 함수가 정리되면 이 줄을 지울 것
+        //cur_pos = -l.width *(int)this.transform.position.y  + (int)this.transform.position.x; //★현재 map[]과 player에서 사용하는 좌표가 서로 다르다, 나중에 통합하고 좌표 갱신 함수가 정리되면 이 줄을 지울 것
+        cur_pos = whereishe();
         Visionchecker.vision_check(cur_pos % l.width, cur_pos / l.width, 6, FOV, l.vision_blockings);
+
 
         //★나중에 단순히 그림자를 씌우고 벗기는 것 이외에 몬스터의 모습을 지우고 다시 나타나게 하는 것까지 넣어줘야 한다, 아니면 그건 몬스터의 vision_searcher에 넣던가
         //프리팹의 RGB값은 0~1 범위로 나타내는 게 기본값같다
@@ -209,8 +224,6 @@ public class player : Thing
             }else{
                 l.temp_gameobjects[i].GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f, 1);
             }
-
-            if (i == cur_pos) { l.temp_gameobjects[i].GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 255); }//★플레이어 좌표를 일시적으로 빨간색으로 색칠하게 했다, 좌표 체계 정리하면 삭제할 것
         } 
     }
 }
