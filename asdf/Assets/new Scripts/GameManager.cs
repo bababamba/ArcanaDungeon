99c1ab2e5051c266f535243b7980636bfa194061
N0rmP <67183361+N0rmP@users.noreply.github.com>
2021-07-20 12:56:09 +0900

    상태이상 추가
    시험 삼아 발화, 기절만 넣어놨읍니다.

---
+++ asdf/Assets/new Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //상태이상 A를 부여할 때는 Thing.condition = Thing.condition | GameManager.A, 상태이상 A를 해제할 때는 Thing.condition = Thing.condition & !GameManager.A, 상태이상 A에 걸렸는지 확인할 때는 if (Thing.condition & GameManager.A != 0) {}
+    public const int burnt = 0x01;
+    public const int stun = 0x02;
+
+}
--- asdf/Assets/new Scripts/Thing.cs
+++ asdf/Assets/new Scripts/Thing.cs
@@ -13,7 +13,12 @@ public class Thing : MonoBehaviour
     protected bool[] FOV;  //Field Of Vision
 
     private string[] text;  //물체의 이름과 설명
-    //Tag 열거형은 잠시 보류, 방법을 좀 더 고민해보기로 했다
+
+    private int condition;  //물체가 보유한 상태이상 및 버프를 나타냄, 각각의 효과들은 GameManager에 상수로 보관되어 있음
+
+    public Thing() {
+        condition = 0;
+    }
 
     //hp 관련 함수
     public int gethp() {
@@ -53,17 +58,34 @@ public class Thing : MonoBehaviour
             this.block -= val;
         }
         else {
-            this.hp -= val - this.block;
+            hpdown(val - this.block);
         }
     }
 
+    //이동 관련 함수
     public void move() { } 
     private void pre_move() { 
         //★move()는 객체마다 달라지지만 pre_move는 달라지지 않으므로 여기서 코딩돼야 한다
         //1.이동해서 도착할 좌표를 가져온다
         //2.이 객체가 목적지에 가기 위해 거쳐야 할 경로를 BPS 알고리즘으로 탐색한다
         //3.그 경로의 좌표들을 배열로 만들어 route_pos에 저장한다
-        //4.move()는 route_pos의 0번 인덱스 좌표로만 이동케 한다
+        //4.move()는 route_pos의 0번 인덱스 좌표로 이동만 하게 한다
+    }
+
+    //상태이상 처리 관련 함수
+    public void condition_process() {
+        if ((this.condition & GameManager.burnt) != 0) {
+            burnt_process();
+        }
+        if ((this.condition & GameManager.stun) != 0) {
+            stun_process();
+        }
+    }
+    private void burnt_process() {
+        hpdown(this.maxhp / 10);
+    }
+    private void stun_process() { 
+        //★턴 생략
     }
 
     public void die() { } //★나중에 자기자신을 map[]에서 삭제하는 정도는 넣어두자
