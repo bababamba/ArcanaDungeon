ca8b7a088085d16d77f92c61a2c4f6cfa13f03ba
geunhuijang <67326548+geunhuijang@users.noreply.github.com>
2021-07-30 16:45:06 +0900

    카드 깔짝
    기획단계가 더 필요하기도 하고 아직까지 제대로 건든게 없음(코드덩어리)

---
+++ asdf/Assets/new Scripts/Cards/CardsSpace.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardSapce
+{
+    public class Cards
+    {
+        public virtual void UseCard(Enemy enemy)
+        {
+            Debug.Log("이게 뜨면 안되는데?");
+        }
+        // 카드에 공통정인 요소
+    }
+
+    public class AttackCard : Cards
+    {
+        private int CardDamege = 10; // 기본 데미지
+
+        public void IncreaseDMG(int DmgUp) // 공격력 강화
+        {
+            CardDamege += DmgUp; 
+        }
+
+        public override void UseCard(Enemy enemy)
+        {
+            enemy.hpdown(CardDamege);
+        }
+    }
+}
+
